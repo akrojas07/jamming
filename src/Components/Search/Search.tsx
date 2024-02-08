c9783fb4a9cd0d5e5250a860d28e99bdab541127
Anakaren <ak.rojas.07@gmail.com>
2024-02-08 10:54:53 -0800

    simplify search component

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,11 +1,29 @@
-import SearchBar from "./SearchBarPresentation";
-import SearchButton from "./SearchButton";
+import Button from "../common/Button/Button";
+import React, {ChangeEvent, MouseEvent, useState} from "react";
 
 export default function Search(){
+    const [search, setSearch] = useState('');
+
+    function handleSearch(e: ChangeEvent<HTMLInputElement>){
+        e.preventDefault();
+        setSearch(e.target.value);
+    }
+
+    function handleClick(e: MouseEvent<HTMLButtonElement>){
+        alert(`${e} I was clicked`)
+    }
+
+    function handleSubmit(){}
+
     return(
-        <>
-            <SearchBar/>
-            <SearchButton />
-        </>
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text" 
+                placeholder="Search"
+                onChange={handleSearch}
+                value={search}
+            />
+            <Button buttonTitle="Search" onClick={handleClick}/>
+        </form>
     )
 }
